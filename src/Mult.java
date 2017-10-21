a99cd9be8fdf17f1419c3126e92ffa69e2149f8a
Romano <Romano@Romano-PC>
2017-10-21 15:41:15 +0200

    Ajout de commentaires + test

--- src/Mult.java
+++ src/Mult.java
@@ -1,18 +1,26 @@
 import java.util.AbstractList;
 import java.util.AbstractSequentialList;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.RandomAccess;
 
 public class Mult {
 
-	public List<Integer> mult(int coefficient, List<Integer> uneListedEntiers) {
+	public static List<Integer> mult(int coefficient, List<Integer> uneListedEntiers) {
 		/*
 		 * Soit la liste contient une implémentation de RandomAccess comme une
 		 * ArrayList par exemple
 		 * 
 		 */
 		if (uneListedEntiers instanceof RandomAccess) {
+			/*
+			 * Ici, on renvoie une AbstractList afin de ne pas allouer une
+			 * nouvelle Liste qui coute bcp en mémoire. On appelle cette Liste
+			 * une "VUE". On utilise le principe de la classe anonyme pour créer
+			 * la vue
+			 */
 			return new AbstractList<Integer>() {
 				@Override
 				public Integer get(int index) {
@@ -108,6 +116,28 @@ public class Mult {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<Integer> al = new ArrayList<>();
+		for (int i = 0; i < 1000000; i++) {
+			al.add(i);
+		}
+		long t0 = System.nanoTime();
+		List<Integer> ret = Mult.mult(2, al);
+		long sum = 0;
+		for (int val : ret) {
+			sum += val / 2;
+		}
+		System.out.println((System.nanoTime() - t0));
+		LinkedList<Integer> ll = new LinkedList<>();
+		for (int i = 0; i < 1000000; i++) {
+			ll.add(i);
+		}
+		t0 = System.nanoTime();
+		sum = 0;
+		ret = Mult.mult(2, ll);
+		for (int val : ret) {
+			sum += val / 2;
+		}
+		System.out.println((System.nanoTime() - t0));
 
 	}
 
